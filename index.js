5c1ad4fd255697b50cd28fe240a47fa9cd0fd206
Hamed Najafi <hamed.najafi.haghi@gmail.com>
2021-06-16 15:21:44 +0430

    a

--- index.js
+++ index.js
@@ -15,7 +15,8 @@ app.use(cors())
 
 
 app.use('/posts', postRoutes)
-const PORT = process.env.NODE_ENV || 5000
+const PORT = process.env.PORT || 5000
+console.log('POOOOORTTTTT is : ', PORT)
 mongoose.connect(process.env.CONNECTION_URL
   , {
     useNewUrlParser: true,
