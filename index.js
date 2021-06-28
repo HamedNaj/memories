8fd2046cec29ae626e1cbf728487f0c3bbf88aec
Hamed Najafi <hamed.najafi.haghi@gmail.com>
2021-06-28 16:13:40 +0430

    Version Auth

--- index.js
+++ index.js
@@ -15,8 +15,6 @@ app.use(bodyParser.urlencoded({limit: '30mb', extended: true}))
 app.use(cors())
 
 
-app.use('/posts', postRoutes)
-app.use('/users', userRoutes)
 const PORT = process.env.PORT || 5000
 mongoose.connect(process.env.CONNECTION_URL
   , {
@@ -32,4 +30,10 @@ mongoose.connect(process.env.CONNECTION_URL
   .catch(err => {
     console.log('error in connecting database ====>>>', err)
   })
+app.use(express.urlencoded());
 app.use(express.static("./client/build"));
+app.use('/posts', postRoutes)
+app.use('/users', userRoutes)
+app.get('*', function (req, res) {
+  res.sendFile('index.html', {root: `./client/build`});
+})
