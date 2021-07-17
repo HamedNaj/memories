416df2c1c38ea0d069413793e95d5fd4ae5765e6
Hamed Najafi <hamed.najafi.haghi@gmail.com>
2021-07-17 22:41:19 +0430

    Version Auth

--- client/src/components/Pagination.jsx
+++ client/src/components/Pagination.jsx
@@ -25,7 +25,7 @@ const Paginate = ({page}) => {
       variant={"outlined"}
       color={'primary'}
       renderItem={(item) => (
-        <PaginationItem {...item} component={Link} to={`/posts?page=${item.page}`}/>
+        <PaginationItem {...item} component={Link} to={`/home?page=${item.page}`}/>
       )}
     />
   )
--- client/src/components/PostDetails/PostDetails.jsx
+++ client/src/components/PostDetails/PostDetails.jsx
@@ -44,8 +44,8 @@ const PostDetails = () => {
           <Typography gutterBottom variant="body1" component="p">{post.message}</Typography>
           <Typography variant="h6">Created by: {post.name}</Typography>
           <Typography variant="body1">{moment(post.createdAt).fromNow()}</Typography>
-          <Divider style={{margin: '20px 0'}}/>
-          <Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>
+          {/*<Divider style={{margin: '20px 0'}}/>*/}
+          {/*<Typography variant="body1"><strong>Realtime Chat - coming soon!</strong></Typography>*/}
           <Divider style={{margin: '20px 0'}}/>
           <CommentSection post={post}/>
           <Divider style={{margin: '20px 0'}}/>
@@ -67,8 +67,8 @@ const PostDetails = () => {
               <div className={classes.recommendedPostsItems} onClick={() => openPost(_id)} key={_id}>
                 <Typography gutterButtom variant={'h6'}>{title}</Typography>
                 <Typography gutterButtom variant={'subtitle2'}>{name}</Typography>
-                <Typography gutterButtom variant={'subtitle2'}>{message}</Typography>
-                <Typography gutterButtom variant={'subtitle1'}>Likes: {likes.length}</Typography>
+                {/*<Typography gutterButtom variant={'subtitle2'}>{message}</Typography>*/}
+                {/*<Typography gutterButtom variant={'subtitle1'}>Likes: {likes.length}</Typography>*/}
                 <img src={selectedFile} width={'200px'}/>
               </div>
             ))}
